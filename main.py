dbf6e9b94daf3af93eae471a13bb740766fddea3
alejandrosalgar <alejandro.salgar@gmail.com>
2026-02-06 19:03:01 -0500

    feat: agregar main

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def sumar_dos_numero(numero1: int, numero2: int) -> int:
 
 
 print(sumar_dos_numero(2, 1))
+
+
+def multiplicar_dos_numero(numero1: int, numero2: int) -> int:
+
+    return numero1 * numero2
